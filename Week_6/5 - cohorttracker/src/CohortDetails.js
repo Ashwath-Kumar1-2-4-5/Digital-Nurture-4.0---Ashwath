c41e16e1cceb1eec1350744cc08ff9e2162eafdf
Ashwath-Kumar1-2-4-5 <ashwathkumar1245@gmail.com>
2025-07-25 17:19:11 +0530

    dlt

--- Week_6/5 - cohorttracker/src/CohortDetails.js
+++ Week_6/5 - cohorttracker/src/CohortDetails.js
@@ -1,27 +1,32 @@
+import React from 'react';
 import styles from './CohortDetails.module.css';
-function CohortDetails(props) {
-    const { cohort } = props;
-    console.log(cohort);
-    const color = cohort.currentStatus.toLowerCase() === 'ongoing' ? 'green' : 'blue';
-    return (
-        <div className={styles.box}>
-            <h3 style={{ color :color}}>
-                {props.cohort.cohortCode} -
-                <span>{props.cohort.technology}</span>
-            </h3>
-            <dl>
-                <dt>Started On</dt>
-                <dd>{props.cohort.startDate}</dd>
-                <dt>Current Status</dt>
-                <dd>{props.cohort.currentStatus}</dd>
-                <dt>Coach</dt>
-                <dd>{props.cohort.coachName}</dd>
-                <dt>Trainer</dt>
-                <dd>{props.cohort.trainerName}</dd>
-            </dl>
-        </div>
+function CohortDetails({ cohort }) {
+  const { name, status, startDate, coach, trainer } = cohort;
+  const nameStyle = {
+    color: status === 'Ongoing' ? 'green' : 'blue', /
+    fontWeight: 'bold', 
+    marginBottom: '8px' 
+  };
 
-        
-    );
+  return (
+    <div className={styles.box}>
+      {/* h3 element displaying the cohort name with conditional inline style */}
+      <h3 style={nameStyle}>{name}</h3>
+      <dl> {/* Definition List to organize key-value pairs of details */}
+        <dt>Started On</dt>
+        <dd>{startDate}</dd> {/* Definition Description for 'Started On' date */}
+
+        <dt>Current Status</dt>
+        <dd>{status}</dd> {/* Definition Description for 'Current Status' */}
+
+        <dt>Coach</dt>
+        <dd>{coach}</dd> {/* Definition Description for 'Coach' */}
+
+        <dt>Trainer</dt>
+        <dd>{trainer}</dd> {/* Definition Description for 'Trainer' */}
+      </dl>
+    </div>
+  );
 }
+
 export default CohortDetails;
